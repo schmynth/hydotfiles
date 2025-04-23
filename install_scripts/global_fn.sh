61085a20b666315d13b7a845767a6d5c8abf464e
schmynth <service-noreply@gmx.de>
2025-04-23 10:25:19 +0200

    FIX: flatpak list path and filename

--- install_scripts/global_fn.sh
+++ install_scripts/global_fn.sh
@@ -7,14 +7,11 @@
 set -e
 
 scrDir="$(dirname "$(realpath "$0")")"
-# cloneDir="$(dirname "${scrDir}")" # fallback, we will use CLONE_DIR now
-# cloneDir="${CLONE_DIR:-${cloneDir}}"
 confDir="${XDG_CONFIG_HOME:-$HOME/.config}"
 cacheDir="${XDG_CACHE_HOME:-$HOME/.cache}/hyde"
 aurList=("yay" "paru")
 shlList=("zsh" "fish")
 
-# export cloneDir
 export confDir
 export cacheDir
 export aurList
