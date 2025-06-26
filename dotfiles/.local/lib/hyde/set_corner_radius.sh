9e69dde938f6e7f880e4e34bf3428d4f70719d51
schmynth <service-noreply@gmx.de>
2025-06-26 10:20:51 +0200

    ADD: superfile corner radius to script

--- dotfiles/.local/lib/hyde/set_corner_radius.sh
+++ dotfiles/.local/lib/hyde/set_corner_radius.sh
@@ -1,28 +1,11 @@
 #!/bin/sh
 
-# set btop corners:
-
-btopConfFile="$HOME/.config/btop/btop.conf"
+confFile="$HOME/.config/btop/btop.conf"
 if [[ $1 == "--zero-radius" ]]; then
-  sed -i '/rounded_corners/c\rounded_corners = False' ${btopConfFile}
+  sed -i '/rounded_corners/c\rounded_corners = False' ${confFile}
 else
-  sed -i '/rounded_corners/c\rounded_corners = True' ${btopConfFile}
+  sed -i '/rounded_corners/c\rounded_corners = True' ${confFile}
 fi
   
-# set superfile corners:
 
-spfConfFile="$HOME/.config/superfile/config.toml"
-
-string="
-# Border style
-border_top = '─'
-border_bottom = '─'
-border_left = '│'
-border_right = '│'
-border_top_left = '╭'
-border_top_right = '╮'
-border_bottom_left = '╰'
-border_bottom_right = '╯'
-border_middle_left = '├'
-border_middle_right = '┤'
-"
+echo $confFile
