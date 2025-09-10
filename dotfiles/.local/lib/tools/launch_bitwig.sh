86658c549c657b249a18767fe2f90b975a9b822f
schmynth <service-noreply@gmx.de>
2025-09-10 13:54:18 +0200

    ADD: pipewire quantum setting to bitwig launch script

--- dotfiles/.local/lib/tools/launch_bitwig.sh
+++ dotfiles/.local/lib/tools/launch_bitwig.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
 set_powersave() {
-	sudo cpupower frequency-set -r -g powersave
-	notify-send "set cpu governor to powersave"
+  sudo cpupower frequency-set -r -g powersave
+
+  pw-metadata -n settings 0 clock.quantum 1024
+
+  notify-send "set cpu governor to powersave and quantum to 1024"
 }
 
 set_performance() {
-	sudo cpupower frequency-set -r -g performance
-	notify-send "set cpu governor to performance"
+  sudo cpupower frequency-set -r -g performance
+
+  pw-metadata -n settings 0 clock.quantum 128
+
+  notify-send "set cpu governor to performance and quantum to 128"
 }
 
-set_performance 
+set_performance
 bitwig-studio
 trap "set_powersave" EXIT
