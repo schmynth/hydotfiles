4eff7ade8027285123bee00095f90940f2e5e725
schmynth <service-noreply@gmx.de>
2025-10-22 21:29:18 +0200

    FIX: launch_bitwig.sh toggle smt

--- dotfiles/.local/lib/tools/launch_bitwig.sh
+++ dotfiles/.local/lib/tools/launch_bitwig.sh
@@ -1,33 +1,29 @@
 #!/bin/bash
 
-set_powersave() {
-  sudo cpupower frequency-set -r -g powersave
-
-  pw-metadata -n settings 0 clock.quantum 1024
-
-  notify-send "set cpu governor to powersave and quantum to 1024"
-}
-
-set_performance() {
-  sudo cpupower frequency-set -r -g performance
-
-  pw-metadata -n settings 0 clock.quantum 128
-
-  notify-send "set cpu governor to performance and quantum to 128"
-}
-
 enable_smt() {
   # enable simultaneous multithreading
   echo on | sudo tee /sys/devices/system/cpu/smt/control
-  notify-send "enable smt"
 }
 
 disable_smt() {
   # disable simultaneous multithreading
   echo off | sudo tee /sys/devices/system/cpu/smt/control
-  notify-send "disable smt"
+}
+
+set_powersave() {
+  sudo cpupower frequency-set -r -g powersave
+  pw-metadata -n settings 0 clock.quantum 1024
+  enable_smt
+  notify-send "set cpu governor to powersave and quantum to 1024, enable smt"
+}
+
+set_performance() {
+  sudo cpupower frequency-set -r -g performance
+  pw-metadata -n settings 0 clock.quantum 128
+  disable_smt
+  notify-send "set cpu governor to performance and quantum to 128, disable_smt"
 }
 
 set_performance
 bitwig-studio
-trap "set_powersave; disable_smt" EXIT
+trap "set_powersave" EXIT
