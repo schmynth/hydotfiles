bb3d63537618f140173addf6666c1f0ddf587d2c
schmynth <service-noreply@gmx.de>
2026-02-16 11:17:46 +0100

    FIX: hyprpm -n flag deprecated

--- install_scripts/install_plugins.sh
+++ install_scripts/install_plugins.sh
@@ -7,8 +7,8 @@
 scrDir=$(dirname "$(realpath "$0")")
 # shellcheck disable=SC1091
 if ! source "${scrDir}/global_fn.sh"; then
-    echo "Error: unable to source global_fn.sh..."
-    exit 1
+  echo "Error: unable to source global_fn.sh..."
+  exit 1
 fi
 
 log_section="Hyprpm"
@@ -18,6 +18,5 @@ print_log -info "Info" -b "[install] :: " "dependencies"
 sudo pacman -S --needed cmake meson pkg-config cpio
 hyprpm update
 hyprpm add https://github.com/KZDKM/Hyprspace || print_log -warn "hyprpm add failed" "could not add repo. Already installed?"
-hyprpm enable Hyprspace  || print_log -warn -r " [add] :: could not enable Hyprspace. Already enabled?"
+hyprpm enable Hyprspace || print_log -warn -r " [add] :: could not enable Hyprspace. Already enabled?"
 hyprpm -v update
-hyprpm reload -nn
